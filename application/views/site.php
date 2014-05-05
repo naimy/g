8de364b5c2a5182f2caa72b077afa0ff8023607f
naimy <jonat.palumbo@gmail.com>
2014-05-05 13:04:36 +0200

    Modif css

--- application/views/site.php
+++ application/views/site.php
@@ -2,9 +2,9 @@
 <h2>Titre du site :</h2>
 <input id="changeTitle" type="text" value="<?php echo $title; ?>" /><input type="submit" value="Modifier" onclick="changeTitle()" />
 <h2>Donnée du site :</h2>
-<ul>
-	<li>Adresse du ts3 : <input id="changeTitle" type="text" value="<?php echo $title; ?>" /><input type="submit" value="Modifier" onclick="changeTitle()" /></li>
-	<li>Adresse du transmission : <input id="changeTitle" type="text" value="<?php echo $title; ?>" /><input type="submit" value="Modifier" onclick="changeTitle()" /></li>
-	<li>Adresse du Plex : <input id="changeTitle" type="text" value="<?php echo $title; ?>" /><input type="submit" value="Modifier" onclick="changeTitle()" /></li>
-	<li>Adresse du server : <input id="changeTitle" type="text" value="<?php echo $title; ?>" /><input type="submit" value="Modifier" onclick="changeTitle()" /></li>
+<ul id="listOfServerFonctinnality">
+	<li><label for="ts3">Adresse du ts3 :</label><input id="ts3" type="text" value="<?php echo $title; ?>" /><input type="submit" value="Modifier" onclick="" /></li>
+	<li><label for="transmission">Adresse du transmission :</label><input id="transmission" type="text" value="<?php echo $title; ?>" /><input type="submit" value="Modifier" onclick="" /></li>
+	<li><label for="plex">Adresse du Plex :</label><input id="plex" type="text" value="<?php echo $title; ?>" /><input type="submit" value="Modifier" onclick="" /></li>
+	<li><label for="server">Adresse du server : </label><input id="server" type="text" value="<?php echo $title; ?>" /><input type="submit" value="Modifier" onclick="" /></li>
 </ul>
