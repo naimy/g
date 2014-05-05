6b045e4c4d77d7707310ebadfbe845fe957ad275
naimy <jonat.palumbo@gmail.com>
2014-05-05 12:48:42 +0200

    ajout liste des fonctionnalité site

--- application/views/site.php
+++ application/views/site.php
@@ -3,12 +3,8 @@
 <input id="changeTitle" type="text" value="<?php echo $title; ?>" /><input type="submit" value="Modifier" onclick="changeTitle()" />
 <h2>Donnée du site :</h2>
 <ul>
-	<li></li>
-	<li></li>
-	<li></li>
-	<li></li>
-	<li></li>
-	<li></li>
-	<li></li>
-	<li></li>
+	<li>Adresse du ts3 :</li>
+	<li>Adresse du transmission :</li>
+	<li>Adresse du Plex :</li>
+	<li>Adresse du server :</li>
 </ul>
