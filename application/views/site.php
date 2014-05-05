5f31f968bbbd5dfb7ecedc618c0092ccf7068f37
naimy <jonat.palumbo@gmail.com>
2014-05-05 12:47:42 +0200

    ajout liste des fonctionnalité site

--- application/views/site.php
+++ application/views/site.php
@@ -1,3 +1,14 @@
 <h1>Gestion du site</h1>
 <h2>Titre du site :</h2>
 <input id="changeTitle" type="text" value="<?php echo $title; ?>" /><input type="submit" value="Modifier" onclick="changeTitle()" />
+<h2>Donnée du site :</h2>
+<ul>
+	<li></li>
+	<li></li>
+	<li></li>
+	<li></li>
+	<li></li>
+	<li></li>
+	<li></li>
+	<li></li>
+</ul>
